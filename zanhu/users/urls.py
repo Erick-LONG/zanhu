999b7b30713ef7694e650cb542d9ab52b8d75894
zhangfulong <834424581@qq.com>
2020-07-07 20:28:57 +0800

    为视图编写测试用例

--- zanhu/users/urls.py
+++ zanhu/users/urls.py
@@ -3,6 +3,6 @@ from zanhu.users import views
 
 app_name = "users"
 urlpatterns = [
-    path("update", views.UserUpdateView.as_view(), name="update"),
+    path("update/", views.UserUpdateView.as_view(), name="update"),
     path("<str:username>/", views.UserDetailView.as_view(), name="detail"),
 ]
