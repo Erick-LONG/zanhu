045eee0ba7b7c500ec8c7492df2cbe599b1a3ccb
zhangfulong <834424581@qq.com>
2020-07-09 21:03:33 +0800

    给文章点赞功能实现

--- zanhu/news/urls.py
+++ zanhu/news/urls.py
@@ -5,5 +5,6 @@ app_name = "news"
 urlpatterns = [
     path("", views.NewsListView.as_view(), name="list"),
     path('post-news/',views.post_new,name='post_news'),
-    path('delete/<str:pk>/',views.NewDeleteView.as_view(),name='delete_news')
+    path('delete/<str:pk>/',views.NewDeleteView.as_view(),name='delete_news'),
+    path('like/',views.like,name='like_post')
 ]
--- zanhu/news/views.py
+++ zanhu/news/views.py
@@ -2,33 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView,DeleteView
+from django.views.generic import ListView, DeleteView
 from django.template.loader import render_to_string
-from django.http import HttpResponse,HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.urls import reverse_lazy
 
 from zanhu.news.models import News
-from zanhu.helpers import ajax_requred,AuthorRequireMixin
+from zanhu.helpers import ajax_requred, AuthorRequireMixin
 
 
-class NewsListView(LoginRequiredMixin,ListView):
+class NewsListView(LoginRequiredMixin, ListView):
     '''首页动态'''
     model = News
     # queryset = News.objects.all()#可做简单过滤
-    paginate_by = 20 #url中的?page=
+    paginate_by = 20  # url中的?page=
     # page_kwarg = 'p'
     # context_object_name = 'news_list' # 要循环的对象默认是「模型类名_list」或者「object_list」
     # ordering = 'created_at' # ('x','y',)在模型类中已经定义
-    template_name = 'news/news_list.html' #可以不用写，默认模型类名_list.html
+    template_name = 'news/news_list.html'  # 可以不用写，默认模型类名_list.html
 
     # def get_ordering(self):
     #     pass
     # def get_paginate_by(self, queryset):
     #     pass
 
-    def get_queryset(self): #可做更复杂的过滤
+    def get_queryset(self):  # 可做更复杂的过滤
         return News.objects.filter(reply=False)
 
     # def get_context_data(self, *, object_list=None, **kwargs):
@@ -38,12 +38,12 @@ class NewsListView(LoginRequiredMixin,ListView):
     #     return context
 
 
-class NewDeleteView(LoginRequiredMixin,AuthorRequireMixin,DeleteView):
+class NewDeleteView(LoginRequiredMixin, AuthorRequireMixin, DeleteView):
     model = News
     template_name = 'news/news_confirm_delete.html'
-    #slug_url_kwarg = 'slug' #通过url传入要删除的对象主键ID，默认值是slug
-    #pk_url_kwarg = 'pk' #通过url传入要删除的对象主键ID，默认值是pk
-    success_url = reverse_lazy('news:list') #在项目URLconf未加载前使用
+    # slug_url_kwarg = 'slug' #通过url传入要删除的对象主键ID，默认值是slug
+    # pk_url_kwarg = 'pk' #通过url传入要删除的对象主键ID，默认值是pk
+    success_url = reverse_lazy('news:list')  # 在项目URLconf未加载前使用
 
 
 @login_required
@@ -54,8 +54,20 @@ def post_new(request):
 
     post = request.POST['post'].strip()
     if post:
-        posted = News.objects.create(user=request.user,content=post)
-        html = render_to_string('news/news_single.html',{'news':posted,'request':request})
+        posted = News.objects.create(user=request.user, content=post)
+        html = render_to_string('news/news_single.html', {'news': posted, 'request': request})
         return HttpResponse(html)
     else:
         return HttpResponseBadRequest('内容不能为空')
+
+
+@login_required
+@ajax_requred
+@require_http_methods(['POST'])
+def like(request):
+    '''点赞,AJAX POST请求'''
+    news_id = request.POST['news']
+    news = News.objects.get(pk=news_id)
+    # 取消或者添加赞
+    news.switch_like(request.user)
+    return JsonResponse({'likes': news.count_likers()})
